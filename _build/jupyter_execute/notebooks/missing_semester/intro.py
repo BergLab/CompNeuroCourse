36500b1e3fa86054b264764e0091e603331a1dfa
Elias Najarro <30030348+enajx@users.noreply.github.com>
2022-11-20 14:56:23 +0100

    update

--- _build/jupyter_execute/notebooks/missing_semester/intro.py
+++
@@ -1,8 +0,0 @@
-#!/usr/bin/env python
-# coding: utf-8
-
-# > "Is my code fast? No. But is it well documented? No. But does it work? Also no." @KyleMorgenstein
-
-# https://missing.csail.mit.edu/ and https://goodresearch.dev/
-
-# 
