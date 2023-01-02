b9b399e8037adc4345f43cbec36c7535b14031a2
Elias Najarro <30030348+enajx@users.noreply.github.com>
2023-01-02 16:34:02 +0100

    update

---
+++ _build/jupyter_execute/notebooks/week0/codingtoolsforresearch.py
@@ -0,0 +1,10 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# # Coding tools for researchers
+
+# > "Is my code fast? No. But is it well documented? No. But does it work? Also no." @KyleMorgenstein
+
+# https://missing.csail.mit.edu/ and https://goodresearch.dev/
+
+# 
